cfbd2ad473c5eb96e9d3e9ec4a6d98ef0604beee
344979026 <344979026@C-H6304V1.tdsb.on.ca>
2019-11-25 13:15:16 -0500

    object height updated

--- ICS3U - Muhammad/src/nasir/unit1/ObjectHeight.java
+++ ICS3U - Muhammad/src/nasir/unit1/ObjectHeight.java
@@ -2,50 +2,42 @@ package nasir.unit1;
 
 import java.util.Scanner;
 
-/**ObjectHeight.Java
- * This program will calculate the height of an object based on specific time
- * September 24,2019
+/**
+ * ObjectHeight.Java This program will calculate the height of an object based
+ * on specific time September 24,2019
+ * 
  * @author Muhammad Nasir
  *
  */
 public class ObjectHeight {
 	/**
-	 * 	Entry point to the program.
+	 * Entry point to the program.
+	 * 
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
 
-		Scanner c= new Scanner(System.in);	
-
+		Scanner c = new Scanner(System.in);
 
 		double time;
 		double height;
 
 		System.out.println("PLease enter a time less than 4.5 seconds: ");
-		time= c.nextDouble();
+		time = c.nextDouble();
 
-		if (time > 4.5 || time <0) {
+		if (time > 4.5 || time < 0) {
 			do {
 				System.out.println("Please eneter valid input ");
-				time= c.nextDouble();
-			}while (time >4.5 || time <0);
+				time = c.nextDouble();
+			} while (time > 4.5 || time < 0);
 		}
 
-		height = 100 - (2.9 *( time*time)); 
-		System.out.println("The height of the object is: "+height+ "meters");
-
+		height = 100 - (2.9 * (time * time));
+		System.out.println("The height of the object is: " + height + "meters");
 
 		/*
 		 * This is the end of the code.
 		 */
 
-
-
-
-
 	}
 }
-
-
-
-
